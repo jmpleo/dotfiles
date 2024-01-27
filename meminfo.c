f6227f5040746dd1b1a967d6df8d525f5563ca02
jmpleo <totrash@duck.com>
2024-01-28 00:02:50 +0300

    some fix

--- meminfo.c
+++ meminfo.c
@@ -16,8 +16,13 @@ int main() {
 
     double memoryPercentage = (double)usedMemory / (double)totalMemory * 100.0;
 
+    int i, n = 8;
+    char load[n + 1]; load[n] = '\0';
+    for (i = 0; i < (int)(memoryPercentage / (100. / n)); ++i) load[i] = '#';
+    for (; i < n; ++i) load[i] = '.';
+
     // printf("%2.2fG (%2.2f%%) / %lldG\n", usedMemory / 1024. / 1024. / 1024., memoryPercentage, totalMemory >> 30);
-    printf("%2.2fG (%2.2f%%)\n", usedMemory / 1024. / 1024. / 1024., memoryPercentage);
+    printf("%2.2fG [%s] (%2.2f%%)\n", usedMemory / 1024. / 1024. / 1024., load, memoryPercentage);
     // printf("\033[1;37m%.2fG (\033[0m%.2f%%\033[1;37m) / %lldG\n\033[0m",
     //         usedMemory / 1024. / 1024. / 1024., memoryPercentage, totalMemory >> 30);
 
